0a9d8d4b9b93263b6f4c85a50d423aacdeef0cb5
Joachim Jablon <ewjoachim@gmail.com>
2026-05-14 12:21:29 +0200

    Switch to httpx

--- tests/acceptance/test_all.py
+++ tests/acceptance/test_all.py
@@ -1,20 +1,22 @@
 from __future__ import annotations
 
+import json
 from pathlib import Path
 
 import pytest
 
 
 @pytest.mark.sphinx(buildername="html", testroot="all")
-def test_build(app, requests_mock, github_payload):
-    github = requests_mock.post("https://api.github.com/graphql", json=github_payload)
+def test_build(app, httpx_mock, github_payload):
+    httpx_mock.add_response(
+        url="https://api.github.com/graphql", method="POST", json=github_payload
+    )
     app.builder.build_all()
     expected = (Path(__file__).parent / "changelog.html").read_text()
 
     received = (app.outdir / "index.html").read_text()
     print(received)
-    assert github.called
-    query = github.request_history[0].json()["query"]
+    query = json.loads(httpx_mock.get_requests()[0].content)["query"]
     assert query.lstrip().startswith("query {")
     assert expected in received
 
